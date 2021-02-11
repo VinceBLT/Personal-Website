e97c81793a363ec6609355e2fc74c3ecbd44cda7
Vincent Bezault <vincent.bezault@icloud.com>
2021-02-11 15:34:32 +0100

    navbar done

--- js/index.js
+++ js/index.js
@@ -44,3 +44,23 @@ document.addEventListener("DOMContentLoaded", function () {
   // On DOM Load initiate the effect
   if (textArray.length) setTimeout(type, newTextDelay + 250);
 });
+
+/* Hamburger Navbar */
+var navbar = document.querySelector(".navbar");
+var ham = document.querySelector(".ham");
+
+// toggles hamburger menu in and out when clicking on the hamburger
+function toggleHamburger() {
+  navbar.classList.toggle("showNav");
+  ham.classList.toggle("showClose");
+}
+
+ham.addEventListener("click", toggleHamburger);
+
+// toggle when clicking on links
+
+// METHOD 1
+var menuLinks = document.querySelectorAll(".menuLink");
+menuLinks.forEach(function (menuLink) {
+  menuLink.addEventListener("click", toggleHamburger);
+});
